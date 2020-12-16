1de1280a18671b5e5688122842a4a85138902452
Zheng-Xiang Ke <kf_ke@hikingbook.net>
2020-12-16 19:58:21 +0800

    Add backgroundColor

--- Sources/ExtendedAlertController/UIAlertController+Helpers.swift
+++ Sources/ExtendedAlertController/UIAlertController+Helpers.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 public extension UIAlertController {
+    var backgroundColor: UIColor? {
+        get {
+            return view.subviews.first?.subviews.first?.subviews.first?.backgroundColor
+        }
+        set(backgroundColor) {
+            view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = backgroundColor
+        }
+    }
+    
     func set(viewController: UIViewController, height: CGFloat? = nil) {
         setValue(viewController, forKey: "contentViewController")
         if let height = height {
