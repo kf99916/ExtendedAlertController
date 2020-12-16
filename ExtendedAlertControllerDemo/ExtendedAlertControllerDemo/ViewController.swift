e8a37af117c215c658407d8955857d074262fc5f
Zheng-Xiang Ke <kf_ke@hikingbook.net>
2020-12-16 19:34:35 +0800

    Demo

--- ExtendedAlertControllerDemo/ExtendedAlertControllerDemo/ViewController.swift
+++ ExtendedAlertControllerDemo/ExtendedAlertControllerDemo/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ExtendedAlertController
 
 class ViewController: UIViewController {
 
@@ -13,7 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+// MARK: @IBAction Methods
+extension ViewController {
+    @IBAction func showTopView(sender: AnyObject) {
+        let alertController = ExtendedAlertController(title: "Top View Alert", message: "😁", preferredStyle: .alert)
+        let imageView = UIImageView(image: UIImage(named: "Check"))
+        alertController.set(topView: imageView, layout: .top)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func showBeyondTopView(sender: AnyObject) {
+        let alertController = ExtendedAlertController(title: "Beyond Top View Alert", message: "😎", preferredStyle: .alert)
+        let imageView = UIImageView(image: UIImage(named: "Check"))
+        alertController.set(topView: imageView, layout: .beyondTop)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
